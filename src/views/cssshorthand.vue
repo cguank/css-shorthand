3c5fa5107b0e186c0b0d89086e9e3d848cf67303
guankunchen <guankunchen@tencent.com>
2020-07-24 15:05:36 +0800

    chore: 上传到git

---
+++ src/views/cssshorthand.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="show-background" ref="ccc">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      这是背景图里面
+    </div>
+    <div class="show-font">
+      这是font
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data: function () {
+    return {};
+  },
+  created () {
+    setTimeout(() => {
+      console.log(window.getComputedStyle(this.$refs.ccc), null);
+    }, 100);
+  },
+  methods: {}
+}
+</script>
+<style scoped>
+.show-background {
+  height: 800px;
+  width: 800px;
+  background-color: red;
+  background: url("../assets/1.jpg") no-repeat top right local;
+}
+.show-font {
+  font: 18px "Avenir", Helvetica, Arial, sans-serif inherit;
+}
+</style>
